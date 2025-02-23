c28ddd5c72f65381d7645d2dab5cca8eac4755d6
nathaliakawanny <161754626+nathaliakawanny@users.noreply.github.com>
2025-02-23 14:37:33 -0300

    Pagamento.java

---
+++ Pagamento.java
@@ -0,0 +1,15 @@
+public class Pagamento {
+    private double valor;
+
+    public Pagamento(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
